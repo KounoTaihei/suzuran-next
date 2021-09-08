469f7adbe0a1ef7b04241815ff40f8c46929b008
河野太平 <>
2021-09-08 16:07:44 +0900

    教室紹介部分作成

---
+++ components/content.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import styles from '../styles/Content.module.scss';
+import Image from 'next/image';
+
+const Content = (props: Props) => {
+
+    // 画像があればimage-fieldを表示する
+    const renderImageField = () => {
+        if(props.image){
+            return (
+                <div className={styles.image_field}>
+                    <Image src={props.image} loading="lazy" />
+                </div>
+            )
+        }
+    }
+
+    return (
+        <div className={`${styles.content} ${styles[props.className]}`}>
+            <h3 className={styles.content_title}>{props.contentTitle}</h3>
+            {/* 画像がある場合はflexクラスを追加 */}
+            <div className={props.image ? `${styles.content_body} ${styles.flex}` : styles.content_body}>
+                {props.image && renderImageField()}
+                <div className={props.image ? `${styles.text_field} ${styles.flex}` : styles.text_field}>
+                    {props.text}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
+
+interface Props {
+    className: string
+    contentTitle: string
+    text: ReactElement
+    image?: StaticImageData
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,48 @@
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Content from '../components/content';
+import shoukaiImage from '../public/fake.webp';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
+  const Shoukai = () => {
+    const text = (
+      <p>
+        「パソコンくらぶ　すずらん」は全国ネットのパソコン教室<a href="http://www.pasokura.com/pasokura/schools/view/192" target="_blank" rel="noreferrer">「パソコンくらぶ <FontAwesomeIcon icon={faExternalLinkAlt} />」</a>の一つとして<span className="under-line">大分市に初めて</span>オープンしました！<br></br><br></br>
+        「パソコン教室って何だか敷居が高い」<br></br>
+        <span className="under-line">「初心者でも大丈夫かな？」</span><br></br>
+        という、あなたのためのパソコン教室です。 <br></br><br></br>
+        全国各地の「パソコンくらぶ」の生徒さんからたくさんの喜びの声をいただいています。 <br></br>
+        「パソコンがだいぶ分かるようになった」<br></br>
+        「パソコンを使えて主人に誉められました♪」<br></br>
+        <span className="under-line">「パソコン教室で覚えた技術で就職が決まりました！」</span> など<br></br><br></br>
+        皆さんもともと秘めているご自身のスキル伸ばしてみませんか？<br></br>
+        大分市付近の皆様！<span className="under-line">まずは遊びにくる感覚でパソコン教室にいらしてください♪</span>
+      </p>
+    )
+
+    return (
+      <>
+        <Content
+            className = 'school-introduction'
+            contentTitle = '教室の紹介'
+            text = {text}
+            image = {shoukaiImage}
+        />
+      </>
+    )
+  }
+
   return (
     <>
       <Head>
         <title>すずらん</title>
       </Head>
-      <h1>suzuran</h1>
+      <div className={styles.home}>
+        <Shoukai />
+      </div>
     </>
   )
 }
